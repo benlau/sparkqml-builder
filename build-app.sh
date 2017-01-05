0c7a1271ef31e0af9f423e90f3ccd742739011eb
Ben Lau <xbenlau@gmail.com>
2017-01-05 17:12:45 +0800

    Add .gitignore and change the build path

--- build-app.sh
+++ build-app.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+export SRCDIR=`pwd`/sparkqml/app/sparkqml
 git clone https://github.com/benlau/sparkqml.git
-cd sparkqml/app/sparkqml
-qpm install
-qmake 
+(cd $SRCDIR;qpm install)
+mkdir -p build/sparkqml
+cd build/sparkqml
+qmake $SRCDIR/sparkqml.pro
 make
 ls
