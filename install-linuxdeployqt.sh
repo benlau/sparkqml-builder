95140779c18fd53441b65d06ec0a73f4cb9e3ffd
Ben Lau <xbenlau@gmail.com>
2017-05-24 23:13:52 +0800

    run

--- install-linuxdeployqt.sh
+++ install-linuxdeployqt.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+set -e
+set -v
+
 wget -c https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage
 mv linuxdeployqt-continuous-x86_64.AppImage linuxdeployqt
 chmod u+x linuxdeployqt
-cp linuxdeployqt/linuxdeployqt $BINDIR
+./linuxdeployqt
+cp linuxdeployqt $BINDIR
 
