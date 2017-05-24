81b5a7323b07ad4d49d52275137162dd26782e5d
Ben Lau <xbenlau@gmail.com>
2017-05-25 00:06:37 +0800

    debug

--- build-linux.sh
+++ build-linux.sh
@@ -7,12 +7,16 @@ sudo add-apt-repository "deb http://us.archive.ubuntu.com/ubuntu vivid main univ
 sudo apt-get update
 sudo apt-get install patchelf
 
+ROOTDIR=`pwd`
 ./qtci/recipes/qt-5.7
 
 cat ./qt-5.7.env
 . ./qt-5.7.env
 
 which qmake
+
+find . -name "libgstaudiodecoder.so"
+
 
 #./build-linuxdeployqt.sh
 ./install-linuxdeployqt.sh
@@ -24,5 +28,6 @@ ldd $BUILDDIR/SparkQML
 
 dpkg -L libgstreamer-plugins-base1.0-0
 ls /usr/lib/x86_64-linux-gnu/libgstaudio-*
+
 
 LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu/:/usr/lib/x86_64-linux-gnu/pulseaudio linuxdeployqt $BUILDDIR/SparkQML -verbose=2 -bundle-non-qt-libs -appimage -qmldir=$SRCDIR
