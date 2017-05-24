2dabb59981a5594b49335838c64ae543944fe8af
Ben Lau <xbenlau@gmail.com>
2017-05-24 23:40:46 +0800

    fix error

--- install-linuxdeployqt.sh
+++ install-linuxdeployqt.sh
@@ -6,6 +6,5 @@ set -v
 wget -c https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage
 mv linuxdeployqt-continuous-x86_64.AppImage linuxdeployqt
 chmod u+x linuxdeployqt
-./linuxdeployqt
 cp linuxdeployqt $BINDIR
 
